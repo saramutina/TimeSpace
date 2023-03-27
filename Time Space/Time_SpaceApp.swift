e44c1cc50721601f20be841e4d8250fd300ad3be
Katie Saramutina <saramutina@gmail.com>
2023-03-28 00:27:36 +0400

    Initial Commit

---
+++ Time Space/Time_SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_SpaceApp.swift
+//  Time Space
+//
+//  Created by Katie Saramutina on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Time_SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
